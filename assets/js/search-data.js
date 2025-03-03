75cef970b116a88b11122eb363697873c153be7c
Arriiaa <66867923+Arriiaa@users.noreply.github.com>
2025-03-03 06:22:05 +0000

    Deploying to gh-pages from @ Arriiaa/arriiaa.github.io@1392a7649ad0178b8b1af3b230bb4cf5931fa2ad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,8 +440,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-隐秘矿洞",
+          title: '隐秘矿洞',
           description: "ARPG 项目关卡白盒",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
