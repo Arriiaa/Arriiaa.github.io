26bef08f3e9365331df33376a6d433bbb4d2d289
Arriiaa <66867923+Arriiaa@users.noreply.github.com>
2025-03-06 23:52:18 +0000

    Deploying to gh-pages from @ Arriiaa/arriiaa.github.io@251123d393a4134941a73fb2d2a8366d1c3c4276 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+        },{id: "post-game-impression-3-assassin-39-s-creed-series",
+      
+        title: "Game Impression #3: Assassin&#39;s Creed series",
+      
+      description: "A Game Analysis of the Combat System in Assassin&#39;s Creed",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/GameImpression1.md/";
+        
+      },
+    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
       
         title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
